4282acfbdbf2b9639c72bef9e1631f0368d8a44d
rcshpark <rcshpark@gmail.com>
2023-04-04 23:39:21 +0900

    불필요 소스코드 제거

--- Netflix.js
+++ Netflix.js
@@ -1,28 +1,28 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-const getHtml = async () => {
+
+let movieList = [];
+async function getHtml() {
   try {
-    return await axios.get("https://www.netflix.com/kr/browse/genre/34399"); // 크롤링할 주소 입력
+    const response = await axios.get("https://www.netflix.com/kr/browse/genre/34399"); 
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const $rankList = $(".nm-collections-row");
+
+    $rankList.each(function (i, element) {
+      const title = $(element).find('span.nm-collections-title-name').text();
+      const image = $(element).find('img.nm-collections-title-img').attr('src');
+      movieList.push({
+        rank: i + 1,
+        title,
+        image,
+      });
+    });
+    return movieList;
   } catch (error) {
     console.error(error);
   }
 };
 
-getHtml()    // 함수 getHtml 생성 > 실행 시, then 수행
-  .then(html => {  // html == 함수
-    let ulList = [];
-    const $ = cheerio.load(html.data);
-    const $bodyList = $(".nm-collections-row");
-    $bodyList.each(function(i) { // each = for문
-      ulList[i] = {
-          title: $(this).find('span.nm-collections-title-name').text(),
-          image: $(this).find('img.nm-collections-title-img').attr('src')
-      };
-    });
-
-    return ulList;
-  })
-  .then(res => {
-	console.log(res)
-  });
+getHtml();
