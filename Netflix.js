b31a054dc66b1cfa78c16d3fe30130f280a12983
rcshpark <rcshpark@gmail.com>
2023-04-05 15:56:04 +0900

    test

--- Netflix.js
+++ Netflix.js
@@ -1,28 +1,59 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-
-let movieList = [];
-async function getHtml() {
+const getHtml = async () => {
   try {
-    const response = await axios.get("https://www.netflix.com/kr/browse/genre/34399"); 
-    const html = response.data;
-    const $ = cheerio.load(html);
-    const $rankList = $(".nm-collections-row");
-
-    $rankList.each(function (i, element) {
-      const title = $(element).find('span.nm-collections-title-name').text();
-      const image = $(element).find('img.nm-collections-title-img').attr('src');
-      movieList.push({
-        rank: i + 1,
-        title,
-        image,
-      });
-    });
-    return movieList;
+    return await axios.get("https://www.netflix.com/kr/browse/genre/34399"); // 크롤링할 주소 입력
   } catch (error) {
     console.error(error);
   }
 };
 
-getHtml();
+getHtml()    // 함수 getHtml 생성 > 실행 시, then 수행
+  .then(html => {  // html == 함수
+    let movieList = [];
+    const $ = cheerio.load(html.data);
+    const $bodyList = $(".nm-content-horizontal-row-item-container").children("li"); // 크롤링할 전체 박스 html 위치 입력
+    $bodyList.each(function(i,element) { // each = for문
+        const title = $(element).find('.nm-collections-title-name').text();
+        movieList.push({
+          title
+        });
+    });
+   
+
+    return movieList;
+  })
+  .then(res => {
+	console.log(res)
+  });
+
+
+
+
+// let movieList = [];
+// async function getHtml() {
+//   try {
+//     const response = await axios.get("https://www.netflix.com/kr/browse/genre/34399"); 
+//     const html = response.data;
+//     const $ = cheerio.load(html);
+//     const $rankList = $(".nm-collections-row");
+
+//     $rankList.each(function (i, element) {
+//       const title = $(element).find('span.nm-collections-title-name').text();
+//       const image = $(element).find('img.nm-collections-title-img').attr('src');
+//       movieList.push({
+//         rank: i + 1,
+//         title,
+//         image,
+//       });
+//     });
+//     return movieList;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
+
+// getHtml();
+
+
